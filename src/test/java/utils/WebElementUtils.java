c43dec951285a2b7a1aa7e562625570a87522d1f
Obitotski <andrew.obitotski@gmail.com>
2022-08-22 23:59:50 +0300

    changes to impl page object

--- src/test/java/utils/WebElementUtils.java
+++ src/test/java/utils/WebElementUtils.java
@@ -1,11 +1,12 @@
 package utils;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import tests.BaseTest;
+import pages.BasePage;
 
 import java.time.Duration;
 
@@ -13,6 +14,11 @@ public class WebElementUtils {
 
     public static boolean checkContentOfElementText(By locator, String expectedText) {
         return waiting(5).until(ExpectedConditions.textToBePresentInElementLocated(locator, expectedText));
+    }
+
+    public static void setValueAndEnter(WebElement element, String value) {
+        setValue(element,value);
+        element.sendKeys(Keys.ENTER);
     }
 
     public static void setValue(WebElement element, String value) {
@@ -24,15 +30,15 @@ public class WebElementUtils {
         return waiting(5).until(ExpectedConditions.presenceOfElementLocated(locator));
     }
 
-    private static boolean isElementPresent(By locator) {
-        return !(driver().findElements(locator).isEmpty());
+    public static boolean isElementPresent(By locator) {
+        try {
+            return !(waiting(3).until(ExpectedConditions.invisibilityOfElementLocated(locator)));
+        } catch (TimeoutException timeoutException) {
+            return true;
+        }
     }
 
     private static WebDriverWait waiting(long timeToWait) {
-        return new WebDriverWait(BaseTest.getDriver(), Duration.ofSeconds(timeToWait));
-    }
-
-    private static WebDriver driver() {
-        return BaseTest.getDriver();
+        return new WebDriverWait(BasePage.getDriver(), Duration.ofSeconds(timeToWait));
     }
 }
